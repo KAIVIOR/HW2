3d4e078086933cd52aa1caeb09b9b3c1c5950111
jasper <cool.daniel1234@gmail.com>
2020-10-17 17:33:03 +0800

    Initial Commit

---
+++ hw2/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  hw2
+//
+//  Created by User02 on 2020/10/17.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
